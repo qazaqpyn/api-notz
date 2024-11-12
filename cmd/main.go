633481c01b84040b39310a28243f805ace6ecd63
qazaqpyn <alimkali.alizhan@gmail.com>
2024-11-12 22:12:44 +0800

    pre setup

---
+++ cmd/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+
+	apinotz "github.com/qazaqpyn/api-notz/server"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+)
+
+func main() {
+	logrus.SetFormatter(new(logrus.JSONFormatter))
+
+	initConfig()
+
+	db, err := repository.NewPostgresDB(viper.GetString("db.url"))
+	if err != nil {
+		logrus.Fatalf("failed to initialize db: %s", err)
+	}
+
+	repos := repository.NewRepository(db)
+
+	// Initialize services
+	services := service.NewService(repos)
+
+	// Initialize handlers
+	handlers := handler.NewHandler(services)
+
+	srv := new(apinotz.Server)
+
+	// Graceful shutdown
+	go func() {
+		if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
+			logrus.Fatalf("error occurred while running http server: %s", err)
+		}
+	}()
+
+	logrus.Print("Server started")
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+	<-quit
+
+	logrus.Print("Server shutting down")
+
+	if err := srv.Shutdown(context.Background()); err != nil {
+		logrus.Errorf("error occurred on server shutting down: %s", err)
+	}
+}
+
+func initConfig() {
+	viper.SetConfigName("config")
+	viper.AddConfigPath("configs")
+	viper.SetConfigType("yml")
+
+	if err := viper.ReadInConfig(); err != nil {
+		logrus.Fatalf("error reading config file: %s", err)
+	}
+}
